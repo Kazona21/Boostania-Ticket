4a362054f376cdfff01f54bb5fdbb2088e04b218
Kazona21 <kaioshin20052005@gmail.com>
2025-08-25 09:56:52 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 
 module.exports = {
     token: '',
-    guildId: '',
-    mongodbURI: ''
+    guildId: '1393558161209950238',
+    mongodbURI: 'mongodb+srv://shiva:shiva@discordbot.opd5w.mongodb.net/?retryWrites=true&w=majority'
 };
+
